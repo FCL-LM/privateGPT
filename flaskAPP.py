3178fcc8e181ca597c066729ca2d17f69d0a74d4
nebuchadneZZar01 <mykael98@live.it>
2023-06-17 17:58:58 +0200

    refactor: privateGPT and api handling enclosed in classes

---
+++ flaskAPP.py
@@ -0,0 +1,45 @@
+import time
+import logging
+from flask import Flask, request, jsonify
+from privateGPT import GPTModel
+
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+
+class FlaskGPT:
+    def __init__(self, gpt: GPTModel) -> None:
+        self.__gpt_model = gpt
+        self.__app = Flask(__name__)
+
+        @self.app.route('/', methods=['POST'])
+        def __query():
+            return self.query()
+
+    @property
+    def app(self):
+        return self.__app
+    
+    @property
+    def gpt_model(self):
+        return self.__gpt_model
+
+    def query(self):
+        answer = {}
+        api_query = request.json['message']
+
+        # Get the answer from the chain
+        start = time.time()
+        answer['answer'] = self.gpt_model.query_model(api_query)
+        end = time.time()
+
+        # Print the result
+        print("\n\n> Question:")
+        print(api_query)
+        print(f"\n> Answer (took {round(end - start, 2)} s.):")
+        
+        return jsonify(answer)
+    
+    def run(self):
+        self.__app.run()
+
+flask = FlaskGPT(GPTModel())
+flask.run()
--- privateGPT.py
+++ privateGPT.py
@@ -1,79 +1,209 @@
-#!/usr/bin/env python3
 from dotenv import load_dotenv
-from langchain.chains import RetrievalQA, ConversationalRetrievalChain
+from langchain.chains import ConversationalRetrievalChain
 from langchain.chains.conversation.memory import ConversationBufferMemory
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.vectorstores import Chroma
 from langchain.llms import GPT4All, LlamaCpp
 import os
-import time
-import logging
-from flask import Flask, request, jsonify
-
-logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
-load_dotenv()
-
-global n_cores
 
 from constants import CHROMA_SETTINGS
 
-app = Flask(__name__)
+class GPTModel:
+    __embeddings: HuggingFaceEmbeddings
+    __db: Chroma
+    __retriever: Chroma.as_retriever
 
-embeddings_model_name = os.environ.get("EMBEDDINGS_MODEL_NAME")
-persist_directory = os.environ.get('PERSIST_DIRECTORY')
+    __llm: None
 
-model_type = os.environ.get('MODEL_TYPE')
-model_path = os.environ.get('MODEL_PATH')
-model_n_ctx = os.environ.get('MODEL_N_CTX')
-model_n_batch = int(os.environ.get('MODEL_N_BATCH',8))
-target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
+    __memory: ConversationBufferMemory
+    __chain: ConversationalRetrievalChain.from_llm
 
-embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
-db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
-retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
+    __last_answer: str
+    __last_source_documents: str
 
-# activate/deactivate the streaming StdOut callback for LLMs
-callbacks = [StreamingStdOutCallbackHandler()]
-n_cores = n_cores = os.environ.get("N_CORES")
-if n_cores is None:
-    n_cores = len(os.sched_getaffinity(0))
 
-# Prepare the LLM
-if model_type == "LlamaCpp":
-    llm = LlamaCpp(model_path=model_path, n_threads=n_cores, n_ctx=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks, verbose=False)
-elif model_type == "GPT4All":
-    llm = GPT4All(model=model_path, n_threads=n_cores, n_ctx=model_n_ctx, backend='gptj', n_batch=model_n_batch, callbacks=callbacks, verbose=False)
-else:
-    print(f"Model {model_type} not supported!")
-    exit
+    def __init__(self) -> None:
+        load_dotenv()
+        self.__embeddings_model_name = os.environ.get("EMBEDDINGS_MODEL_NAME")
+        self.__persist_directory = os.environ.get('PERSIST_DIRECTORY')
 
-# Preparing the memory-byffered chain
-memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True, output_key='answer')                     # buffer containing the entire conversation history
-qa = ConversationalRetrievalChain.from_llm(llm=llm, retriever=retriever, memory=memory, return_source_documents=True)
+        self.__model_type = os.environ.get('MODEL_TYPE')
+        self.__model_path = os.environ.get('MODEL_PATH')
+        self.__model_n_ctx = os.environ.get('MODEL_N_CTX')
+        self.__model_n_batch = int(os.environ.get('MODEL_N_BATCH',8))
+        self.__target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
 
-# Questions and answers via REST apis
-@app.route('/', methods=['POST'])
-def query():
-    answer = {}
-    api_query = request.json['message']
+        self.__n_cores = os.environ.get("N_CORES")
+        if self.__n_cores is None:
+            self.__n_cores = len(os.sched_getaffinity(0))
 
-    # Get the answer from the chain
-    start = time.time()
-    res = qa(api_query)
-    answer['answer'], docs = res['answer'], res['source_documents']
-    end = time.time()
+        # activate/deactivate the streaming StdOut callback for LLMs
+        self.__callbacks = [StreamingStdOutCallbackHandler()]
 
-    # Print the result
-    print("\n\n> Question:")
-    print(api_query)
-    print(f"\n> Answer (took {round(end - start, 2)} s.):")
+        self.init_model()
+    
 
-    # Print the relevant sources used for the answer
-    for document in docs:
-        print("\n> " + document.metadata["source"] + ":")
-        print(document.page_content)
+    # ----- PROPERTIES -----
 
-    return jsonify(answer)
+    # ---- ENV VARIABLES ----
+    @property
+    def embeddings_model_name(self):
+        return self.__embeddings_model_name
+    
+    @property
+    def persist_directory(self):
+        return self.__persist_directory
+    
+    @property
+    def model_type(self):
+        return self.__model_type
+    
+    @property
+    def model_path(self):
+        return self.__model_path
+    
+    @property
+    def model_n_ctx(self):
+        return self.__model_n_ctx
 
-app.run()
+    @property
+    def model_n_batch(self):
+        return self.__model_n_batch
+    
+    @property
+    def target_source_chunks(self):
+        return self.__target_source_chunks
+    
+    @property
+    def n_cores(self):
+        return self.__n_cores
+    
+    
+    # llm internal data-structures 
+    @property
+    def embeddings(self):
+        return self.__embeddings
+    
+    @property
+    def callbacks(self):
+        return self.__callbacks
+    
+    @embeddings.setter
+    def embeddings(self, embeds: HuggingFaceEmbeddings):
+        self.__embeddings = embeds
+
+    @property
+    def database(self):
+        return self.__db
+
+    @database.setter
+    def database(self, db_chroma: Chroma):
+        self.__db = db_chroma
+
+    @property
+    def retriever(self):
+        return self.__retriever
+    
+    @retriever.setter
+    def retriever(self, retriever: Chroma.as_retriever):
+        self.__retriever = retriever
+    
+    @property
+    def language_model(self):
+        return self.__llm
+    
+    @language_model.setter
+    def language_model(self, new_llm):
+        self.__llm = new_llm
+
+    @property
+    def memory_buffer(self):
+        return self.__memory
+    
+    @memory_buffer.setter
+    def memory_buffer(self, buffer: ConversationBufferMemory):
+        self.__memory = buffer
+
+    @property
+    def retrivial_chain(self):
+        return self.__chain
+
+    @retrivial_chain.setter
+    def retrivial_chain(self, new_chain):
+        self.__chain = new_chain
+
+
+    # ---- FINAL OUTPUT VARIABLES ----
+    # inference
+    @property
+    def answer(self):
+        return self.__last_answer
+    
+    @answer.setter
+    def answer(self, inference: str):
+        self.__last_answer = inference
+    
+    # source documents
+    @property
+    def source_documents(self):
+        return self.__last_source_documents
+    
+    @source_documents.setter
+    def source_documents(self, source_docs: str):
+        self.__last_source_documents = source_docs
+    
+    # ----- METHODS -----
+    # Prepare embeddings, database and retriever
+    def load_embeddings_retriever(self):
+        self.__embeddings = HuggingFaceEmbeddings(model_name = self.embeddings_model_name)
+        self.__db = Chroma(persist_directory=self.persist_directory, embedding_function=self.embeddings, client_settings=CHROMA_SETTINGS)
+        self.__retriever = self.database.as_retriever(search_kwargs={"k": self.target_source_chunks})
+    
+
+    # Prepare the LLM
+    def load_llm_model(self):
+        if self.model_type == "LlamaCpp":
+            self.language_model = LlamaCpp(model_path = self.model_path,\
+                                            n_threads = self.n_cores,\
+                                            n_ctx = self.model_n_ctx,\
+                                            n_batch = self.model_n_batch,\
+                                            callbacks = self.callbacks,\
+                                            verbose = False)
+        elif self.model_type == "GPT4All":
+            self.language_model = GPT4All(model = self.model_path,\
+                                            n_threads = self.n_cores,\
+                                            n_ctx = self.model_n_ctx,\
+                                            backend = 'gptj',\
+                                            n_batch = self.model_n_batch,\
+                                            callbacks = self.callbacks,\
+                                            verbose = False)
+        else:
+            print(f"Model {self.model_type} not supported!")
+            exit
+
+
+    # Preparing the memory-buffered chain
+    def load_memory_chain(self):
+        self.memory_buffer = ConversationBufferMemory(memory_key='chat_history', return_messages=True, output_key='answer')                     # buffer containing the entire conversation history
+        self.retrivial_chain = ConversationalRetrievalChain.from_llm(llm = self.language_model, retriever = self.retriever,\
+                                                                     memory=self.memory_buffer, return_source_documents=True)
+        
+
+    def init_model(self):
+        self.load_embeddings_retriever()
+        self.load_llm_model()
+        self.load_memory_chain()
+
+
+    # Query the LLM model with a string
+    def query_model(self, query: str):
+        result = self.retrivial_chain(query)
+        self.answer, self.source_documents = result['answer'], result['source_documents']
+
+        # Print the relevant sources used for the answer
+        for document in self.source_documents:
+            print("\n> " + document.metadata["source"] + ":")
+            print(document.page_content)
+
+        return self.answer
