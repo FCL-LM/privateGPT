264b3c34e472c5234eaa46524101bc248fa0227b
nebuchadneZZar01 <mykael98@live.it>
2023-06-18 15:39:54 +0200

    fix: added s3 key environment variables

--- s3_override.py
+++ s3_override.py
@@ -8,10 +8,9 @@ from langchain.docstore.document import Document
 from langchain.document_loaders.base import BaseLoader
 from langchain.document_loaders.unstructured import UnstructuredFileLoader
 
-credentials_file = os.environ.get('CREDENTIALS_FILE', 'credentials.json')
-endpoint_url = os.environ.get('ENDPOINT_URL', 'http://localhost:9000')
-
-s3_key = json.load(open(credentials_file))
+access_key = os.environ.get('ACCESS_KEY', 'adminadmin')
+secret_key = os.environ.get('SECRET_KEY', 'adminadmin')
+endpoint_url = os.environ.get('ENDPOINT_URL', 'http://localhost:8333')
 
 class S3FileLoader(BaseLoader):
     """Loading logic for loading documents from s3."""
@@ -31,22 +30,19 @@ class S3FileLoader(BaseLoader):
                 "Please install it with `pip install boto3`."
             )
         
-        if s3_key:
-            s3 = boto3.client(use_ssl = True,\
-                                service_name = 's3',\
-                                aws_access_key_id = s3_key.get('accessKey'),\
-                                aws_secret_access_key = s3_key.get('secretKey'),\
-                                endpoint_url = endpoint_url)
-            
-            with tempfile.TemporaryDirectory() as temp_dir:
-                file_path = f"{temp_dir}/{self.key}"
-                os.makedirs(os.path.dirname(file_path), exist_ok=True)
-                s3.download_file(self.bucket, self.key, file_path)
-                loader = UnstructuredFileLoader(file_path)
-                return loader.load()
-        else:
-            raise Exception('No credential file has been found,\
-                            cannot load file from S3 bucket')
+        s3 = boto3.client(use_ssl = True,\
+                            service_name = 's3',\
+                            aws_access_key_id = access_key,\
+                            aws_secret_access_key = secret_key,\
+                            endpoint_url = endpoint_url)
+        
+        with tempfile.TemporaryDirectory() as temp_dir:
+            file_path = f"{temp_dir}/{self.key}"
+            os.makedirs(os.path.dirname(file_path), exist_ok=True)
+            s3.download_file(self.bucket, self.key, file_path)
+            loader = UnstructuredFileLoader(file_path)
+            return loader.load()
+
 
 class S3DirectoryLoader(BaseLoader):
     """Loading logic for loading documents from s3."""
@@ -68,24 +64,21 @@ class S3DirectoryLoader(BaseLoader):
         
         # Read variables from environment and exit in case of empty string
         
-        if s3_key:
-            s3 = boto3.client(use_ssl = True,\
-                                service_name = 's3',\
-                                aws_access_key_id = s3_key.get('accessKey'),\
-                                aws_secret_access_key = s3_key.get('secretKey'),\
-                                endpoint_url = endpoint_url)
-            
-            s3_objs = s3.list_objects_v2(Bucket=self.bucket)
-            docs = []
+        s3 = boto3.client(use_ssl = True,\
+                            service_name = 's3',\
+                            aws_access_key_id = access_key,\
+                            aws_secret_access_key = secret_key,\
+                            endpoint_url = endpoint_url)
+        
+        s3_objs = s3.list_objects_v2(Bucket=self.bucket)
+        docs = []
 
-            if "Contents" in s3_objs:
-                for obj in s3_objs.get('Contents'):
-                    loader = S3FileLoader(self.bucket, obj.get('Key'))
-                    docs.extend(loader.load())
-            else:
-                print("The bucket is empty.")
-
-            return docs
+        if "Contents" in s3_objs:
+            for obj in s3_objs.get('Contents'):
+                loader = S3FileLoader(self.bucket, obj.get('Key'))
+                docs.extend(loader.load())
         else:
-            raise Exception('No credential file has been found,\
-                            cannot load directory from S3 bucket')
+            print("The bucket is empty.")
+
+        return docs
+
