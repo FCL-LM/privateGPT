cdeb38ece71d31d215bce1dd2781b07b9bb1526b
nebuchadneZZar01 <mykael98@live.it>
2023-06-18 22:59:34 +0200

    refactor: cleaned code, added comments, updated variables and methods to make more understandable

--- ingest.py
+++ ingest.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import os
+import os, sys
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import scan as elastic_scan
 import s3_override 
@@ -34,7 +34,8 @@ embeddings_model_name = os.environ.get('EMBEDDINGS_MODEL_NAME')
 chunk_size = 500
 chunk_overlap = 50
 
-s3_source_directory = s3_override.S3DirectoryLoader("sources")
+# Load the s3 bucket containing the documents to ingest
+s3_source_loader = s3_override.S3DirectoryLoader("sources")
 
 # Custom document loaders
 class MyElmLoader(UnstructuredEmailLoader):
@@ -78,94 +79,101 @@ LOADER_MAPPING = {
     # Add more mappings for other file extensions and loaders as needed
 }
 
+# Retrieves only the new documents that are not already in VectorStore
+def filter_documents(vector_docs: List[str]) -> List[Document]:
+    print(f"Loading documents from bucket s3://{s3_source_loader.bucket}")
+    # Take documents from s3 bucket
+    documents = s3_source_loader.load()
 
-def load_single_document(file_path: str) -> List[Document]:
-    ext = "." + file_path.rsplit(".", 1)[-1]
-    if ext in LOADER_MAPPING:
-        loader_class, loader_args = LOADER_MAPPING[ext]
-        loader = loader_class(file_path, **loader_args)
-        return loader.load()
-
-    raise ValueError(f"Unsupported file extension '{ext}'")
-
-def load_documents(ignored_files: List[str]) -> List[Document]:
-    print(f"Loading documents from bucket s3://{s3_source_directory.bucket}")
-    documents = s3_source_directory.load()
     if not documents:
         print("No new documents to load")
         sys.exit(0)
-    
-    filtered_files = [f for f in documents if os.path.basename(f.dict()['metadata']['source']) not in ignored_files]
-    return filtered_files
+
+    # Control which docs have been already processed 
+    new_docs = []
+    for doc in documents:
+        doc_source = os.path.basename(doc.dict()['metadata']['source'])
+        if doc_source not in vector_docs:
+            new_docs.append(doc)
+
+    return new_docs
 
 
-def process_documents(ignored_files: List[str] = []) -> List[Document]:
-    """
-    Load documents and split in chunks
-    """
-    documents = load_documents(ignored_files)
-    print(f"Loaded {len(documents)} new documents from bucket s3://{s3_source_directory.bucket}")
+# Load new documents and split in chunks
+# if vector_docs is empty, then documents are loaded from the s3 bucket
+def process_documents(vector_docs: List[str] = []) -> List[Document]:
+    new_documents = filter_documents(vector_docs)
+    print(f"Loaded {len(new_documents)} new documents from bucket s3://{s3_source_loader.bucket}")
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-    texts = text_splitter.split_documents(documents)
+    texts = text_splitter.split_documents(new_documents)
     print(f"Split into {len(texts)} chunks of text (max. {chunk_size} tokens each)")
+    
     return texts
 
+
+# Checks if vectorstore exists
 def does_vectorstore_exist() -> bool:
-    """
-    Checks if vectorstore exists
-    """
     es = Elasticsearch(elastic_endpoint)
 
     if es.indices.exists(index=elastic_index):
         res = es.cat.count(index=elastic_index)
+        n_docs = int(res[2])
 
-        if int(res[2]) > 3:
+        # At least 3 docs are needed in a working vectorstore
+        if n_docs > 3:
             return True
 
     return False
 
-def get_sources_metadata() -> set:
+
+# Retrievies list of the documents in VectorStore
+def get_vector_documents() -> set:
     es = Elasticsearch(elastic_endpoint)
 
     es_response = elastic_scan(
         es,
-        index=elastic_index,
-        query={"query": { "match_all" : {}}},
+        index = elastic_index,
+        query = {"query": { "match_all" : {} }}
     )
 
-    sources = set()
+    # List of all documents already in VectorStore
+    vector_files = []
 
-    for d in es_response:
-        sources.add(os.path.basename(d['_source']['metadata']['source']))
+    for record in es_response:
+        record_source = record['_source']['metadata']['source']
+        file_bn = os.path.basename(record_source)
 
-    return sources
+        vector_files.append(file_bn)
+
+    return vector_files
+
 
 def main():
     # Create embeddings
-    embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
+    embeddings = HuggingFaceEmbeddings(model_name = embeddings_model_name)
 
     if does_vectorstore_exist():
-        # Update and store locally vectorstore
+        # Update and store vectorstore on ElasticSearch DB
         print(f"Appending to existing vectorstore at {elastic_index}")
-        db =  ElasticVectorSearch(elasticsearch_url=elastic_endpoint,\
-                                        index_name=elastic_index,\
-                                        embedding=embeddings)
+        db =  ElasticVectorSearch(elasticsearch_url = elastic_endpoint,\
+                                    index_name = elastic_index,\
+                                    embedding = embeddings)
         
-        sources = get_sources_metadata()
-
-        texts = process_documents(list(sources))
-
+        vector_docs = get_vector_documents()
+        texts = process_documents(vector_docs)
         if len(texts) > 0:
             print(f"Creating embeddings. May take some minutes...")
             db.add_documents(texts)
     else:
-        # Create and store locally vectorstore
+        # Create and store VectorStore on ElasticSearch DB
         print("Creating new vectorstore")
         texts = process_documents()
         print(f"Creating embeddings. May take some minutes...")
-        db = ElasticVectorSearch.from_documents(texts, embeddings, elasticsearch_url=elastic_endpoint, index_name=elastic_index)
+        db = ElasticVectorSearch.from_documents(texts, embeddings,\
+                                                elasticsearch_url = elastic_endpoint,\
+                                                index_name = elastic_index)
 
-    print(f"Ingestion complete! You can now run privateGPT.py to query your documents")
+    print(f"Ingestion complete! You can now run privateGPT to query your documents")
 
 
 if __name__ == "__main__":
