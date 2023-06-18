99353128057d65f848f556681b45a277f88fd0e6
josura <giorgiolocicero@hotmail.com>
2023-06-19 01:51:51 +0200

    plotting code for R and standardized questions for the models

---
+++ plottingPerformance.R
@@ -0,0 +1,15 @@
+library(ggplot2)
+
+timeSeries <- read.csv("/home/josura/Projects/LLM/privateGPT/time.csv",sep = ",",header = FALSE)
+
+colnames(timeSeries) <- c("number_of_cores", "modelWeights", "modelEmbeddings", "time")
+
+timeSeries$modelDetails <- paste(timeSeries$modelWeights, timeSeries$modelEmbeddings, sep = ":")
+
+
+ggplot(timeSeries, aes(x = modelDetails, y = time, fill = modelDetails)) +
+  geom_violin(scale = "width") +
+  scale_fill_viridis_d() +
+  labs(x = "Combined Feature (Model Weights : Model Embeddings)", y = "Time") + 
+  geom_boxplot(width=0.1)+
+  theme(axis.text.x = element_blank())
