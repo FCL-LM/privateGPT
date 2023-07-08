f23d4f961e2ef41bdda6972906e2434f18d58f90
josura <giorgiolocicero@hotmail.com>
2023-07-08 12:02:04 +0200

    answers to questions for chatGPT

--- plottingPerformance.R
+++ plottingPerformance.R
@@ -13,3 +13,16 @@ ggplot(timeSeries, aes(x = modelDetails, y = time, fill = modelDetails)) +
   labs(x = "Combined Feature (Model Weights : Model Embeddings)", y = "Time") + 
   geom_boxplot(width=0.1)+
   theme(axis.text.x = element_blank())
+
+library(dplyr)
+timeSeries.filtered <- timeSeries %>%
+  select(time,modelDetails,modelWeights) %>%
+  filter(modelWeights == "GPU/LlamaCppmodels/nous-hermes-13b.ggmlv3.q4_0.bin" | modelWeights == "LlamaCppmodels/nous-hermes-13b.ggmlv3.q4_0.bin")
+
+
+ggplot(timeSeries.filtered, aes(x = modelDetails, y = time, fill = modelDetails)) +
+  geom_violin(scale = "width") +
+  scale_fill_viridis_d() +
+  labs(x = "Combined Feature (Model Weights : Model Embeddings)", y = "Time") + 
+  geom_boxplot(width=0.1)+
+  theme(axis.text.x = element_blank())
